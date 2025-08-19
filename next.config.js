35d217075a205eb1ad30619513235021b3c9020c
Padu76 <andrea.padoan@gmail.com>
2025-08-19 02:48:16 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -65,7 +65,7 @@ const nextConfig = {
           },
           {
             key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
+            value: 'Content-Type, Authorization, stripe-signature',
           },
         ],
       },
@@ -83,14 +83,9 @@ const nextConfig = {
     ];
   },
 
-  // Rewrites per API esterne
+  // Rewrites - Rimuovo il rewrite problematico
   async rewrites() {
-    return [
-      {
-        source: '/api/stripe-webhook',
-        destination: '/api/webhook',
-      },
-    ];
+    return [];
   },
 
   // Variabili d'ambiente da esporre al client
@@ -118,13 +113,122 @@ const nextConfig = {
     dirs: ['app', 'components', 'lib'],
   },
 
-  // Experimental features
+  // Experimental features - Aggiungo i pacchetti Firebase
   experimental: {
-    serverComponentsExternalPackages: ['firebase-admin'],
+    serverComponentsExternalPackages: [
+      'firebase-admin',
+      '@firebase/app',
+      '@firebase/auth',
+      '@firebase/firestore',
+      '@firebase/storage',
+      'firebase'
+    ],
   },
 
   // Output
   output: 'standalone',
-};
+
+  // WEBPACK CONFIGURATION - FIX per undici/Firebase
+  webpack: (config, { isServer }) => {
+    // Fix per il problema di undici con sintassi private fields
+    config.module.rules.push({
+      test: /\.js$/,
+      include: /node_modules\/undici/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-proposal-private-methods',
+            '@babel/plugin-proposal-private-property-in-object'
+          ]
+        }
+      }
+    });
+
+    // Alternativa: escludi completamente undici dal bundle client
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'undici': false,
+        'undici/lib/web/fetch/util': false,
+        'undici/lib/web/fetch/headers': false,
+      };
+    }
+
+    // Fallback per moduli Node.js nel browser
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      crypto: false,
+      stream: false,
+      url: false,
+      zlib: false,
+      http: false,
+      https: false,
+      assert: false,
+      os: false,
+      path: false,
+      util: false,
+      events: false,
+      buffer: false,
+      process: false,
+    };
+
+    // Ignora warnings per moduli opzionali
+    config.ignoreWarnings = [
+      { module: /node_modules\/undici/ },
+      { module: /node_modules\/@firebase/ },
+      { module: /node_modules\/firebase/ },
+    ];
+
+    // Ottimizzazione per Firebase
+    if (!isServer) {
+      config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+          chunks: 'all',
+          cacheGroups: {
+            firebase: {
+              name: 'firebase',
+              test: /[\\/]node_modules[\\/](firebase|@firebase)[\\/]/,
+              priority: 10,
+            },
+            commons: {
+              name: 'commons',
+              minChunks: 2,
+              priority: 5,
+              reuseExistingChunk: true,
+            },
+          },
+        },
+      };
+    }
+
+    return config;
+  },
+
+  // Transpile packages - Aggiungo i moduli da transpilare
+  transpilePackages: [
+    'firebase',
+    '@firebase/app',
+    '@firebase/auth',  
+    '@firebase/firestore',
+    '@firebase/storage',
+    '@firebase/functions',
+    '@firebase/database',
+    '@firebase/analytics',
+    '@firebase/performance',
+    '@firebase/remote-config',
+    '@firebase/messaging',
+    '@firebase/util',
+    '@firebase/component',
+    'emailjs-com',
+    '@emailjs/browser'
+  ],
+}
 
 module.exports = nextConfig;
